bf246c3d1bb9df02c2d89ba0fd2bf49ffd1fbdf3
SeanHolden <sean.holden@o2.com>
2013-08-05 14:42:03 +0100

    removed ruby code that I was using as a guide

--- app.js
+++ app.js
@@ -1,5 +1,8 @@
 var express = require('express');
 var app = express();
+var key = process.env.TOKBOX_API_KEY;    // Replace with your API key  
+var secret = process.env.TOKBOX_SECRET;  // Replace with your API secret  
+var opentok = new OpenTok.OpenTokSDK(key, secret);
 
 app.get('/', function(req, res){
   var body = '<h1>Hello World</h1>';
