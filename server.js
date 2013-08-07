5554cd7594487140a187c5dff48235219241f4c6
SeanHolden <sean.holden@o2.com>
2013-08-07 14:54:49 +0100

    handled different port

--- server.js
+++ server.js
@@ -28,6 +28,6 @@ app.get('/', function(request, response){
     response.end();
   });
 });
-
-app.listen(3000);
-console.log('Listening on port 3000 in %s environment', process.env.NODE_ENV);
+var port = process.env.PORT || 3000;
+app.listen(port || 3000);
+console.log('Listening on ' + port);
